100988dc008c6ae0778b8d4e517319b1319c1729
riflab <arif.darmawan29@gmail.com>
2023-02-10 18:43:37 +0700

    update

--- src/main.py
+++ src/main.py
@@ -1,12 +1,35 @@
 from bs4 import BeautifulSoup as soup
 import requests
+import re
 
-
-def main(url):
+def parse_url(url):
     response = requests.get(url)
     page_soup = soup(response.text, "html.parser")
+    page = page_soup.find_all('a', href=True)
 
-    print(page_soup)
+    list = [item['href'] for item in page]
+
+    return list
+
+def main(url):
+    
+    pages_1 = parse_url(url)[-2:]
+    
+    for i in pages_1:
+        url_1 = url + i
+        print(' > ' + url_1)
+
+        pages_2 = parse_url(url_1)
+
+        for j in pages_2[5:]:
+            url_2 = url_1 + j
+            print(' >> ' + url_2)
+
+            pages_3 = parse_url(url_2)
+
+            for k in pages_3[5:]:
+                url_3 = url_2 + k
+                print(' >>> ' + url_3)
 
 if __name__ == '__main__':
     # headers, cookies = hd()
